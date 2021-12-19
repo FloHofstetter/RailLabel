411f5e11646c48c9c6e3f82343d4cde14127130f
Florian Hofstetter <flo.max.hofstetter@gmail.com>
2021-12-19 14:22:25 +0100

    Path configuration in function; Introduce settings.yml

--- rail_label/__main__.py
+++ rail_label/__main__.py
@@ -1,9 +1,18 @@
 import pathlib
 import argparse
 from rail_label.label_gui import LabelGui
+import yaml
 
 
-def parse_args(parser: argparse.ArgumentParser):
+def parse_yaml():
+    """
+    Parse configuration from YAML.
+    :return:
+    """
+    data = yaml.parse()
+
+
+def parse_args(parser: argparse.ArgumentParser) -> argparse.Namespace:
     """
     Parse CLI arguments.
     :param parser: Argument parser Object.
@@ -16,19 +25,42 @@ def parse_args(parser: argparse.ArgumentParser):
         help="Path to the directory containing a dataset.",
         default=".",
     )
+    parser.add_argument(
+        "-s",
+        "--settings_path",
+        type=str,
+        help="Path to settings YAML-file for RailLabel.",
+    )
     return parser.parse_args()
 
 
+def configure_paths(args: argparse.Namespace) -> dict[str, pathlib.Path]:
+    """
+    Set up paths.
+    :param args: CLI arguments
+    :return: Dictionary containing paths
+    """
+    paths = {}
+    # Settings YAML
+    if args.settings_path:
+        paths["settings"] = pathlib.Path(args.settings_path)
+    else:
+        paths["settings"] = pathlib.Path("settings.yml")
+    # Dataset
+    paths["data_set"] = pathlib.Path(args.dataset_path)
+
+    return paths
+
+
 def main():
-    # Parse arguments from cli
     parser = argparse.ArgumentParser()
     args = parse_args(parser)
 
-    # Path parameters
-    data_set_path = pathlib.Path(args.dataset_path)
+    paths: dict[str, pathlib.Path]
+    paths = configure_paths(args)
 
     # RailLabel main object
-    label_gui = LabelGui(data_set_path)
+    label_gui = LabelGui(paths["data_set"])
     label_gui.event_loop()
 
 
