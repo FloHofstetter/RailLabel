899ba9bd6901f8e8d9dca044aee238471a8b1378
Florian Hofstetter <flo.max.hofstetter@gmail.com>
2021-12-24 14:06:20 +0100

    Typehints and fixes in the point module

--- src/rail_label/labeling/scene/point.py
+++ src/rail_label/labeling/scene/point.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Union
 import numpy as np
 
@@ -7,32 +8,31 @@ class ImagePoint:
     Represents a point in image coordinates aka. pixel coordinates.
     """
 
-    def __init__(self, x: Union[int, float], y: Union[int, float]):
+    def __init__(self, x: Union[int, float], y: Union[int, float]) -> None:
         """
         :param x: X coordinate
         :param y: Y coordinate
-        :return: None
         """
         self._point: np.ndarray
         self._point = np.rint(np.array([x, y])).astype(int)
 
-    def __str__(self):
-        msg = f"X:{self.x} Y:{self.y}"
-        return str(msg)
+    def __str__(self) -> str:
+        msg: str = f"X={self.x} Y={self.y}"
+        return msg
 
     @property
-    def point(self):
+    def point(self) -> int:
         return self._point
 
     @property
-    def x(self):
+    def x(self) -> int:
         return self._point[0].item()
 
     @property
-    def y(self):
+    def y(self) -> int:
         return self._point[1].item()
 
-    def midpoint(self, other: "ImagePoint"):
+    def midpoint(self, other: ImagePoint) -> ImagePoint:
         """
         Calculate midpoint between this point and other point.
         :param other: Other point
@@ -40,7 +40,7 @@ class ImagePoint:
         """
         mean: np.ndarray = np.mean((self._point, other._point), axis=0)
         # Pixels are discrete values
-        mean = np.rint(mean).astype(int)
+        mean: np.ndarray = np.rint(mean).astype(int)
         midpoint: ImagePoint = self.__class__(mean[0], mean[1])
         return midpoint
 
@@ -50,23 +50,27 @@ class WorldPoint(ImagePoint):
     Represents a point in word coordinates.
     """
 
-    def __init__(self, x: int, y: int, z: int):
+    def __init__(
+        self,
+        x: Union[int, float],
+        y: Union[int, float],
+        z: Union[int, float],
+    ) -> None:
         """
         :param x: X coordinate
         :param y: Y coordinate
         :param z: Z coordinate
-        :return: None
         """
         super().__init__(x, y)
-        self._point = np.append(self._point, z)
+        self._point = np.append(self._point, round(z))
 
-    def __str__(self):
-        msg = f"X:{self.x} Y:{self.y} Z:{self.z}"
-        return str(msg)
+    def __str__(self) -> str:
+        msg: str = f"X={self.x} Y={self.y} Z={self.z}"
+        return msg
 
     @property
-    def z(self):
-        return self._point[2]
+    def z(self) -> int:
+        return self._point[2].item()
 
 
 def main():
